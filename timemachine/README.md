d4b7b7ac0165749c2d76dbf676f45a9f35517d82
Daniel Hazelbaker <daniel@blueboxmoon.com>
2014-04-19 18:35:05 -0700

    Add the TimeMachine plugin.

---
+++ timemachine/README.md
@@ -0,0 +1,29 @@
+# TimeMachine status plugin for Sal.
+
+## Client Script
+
+The client script is an external facter script, which means it needs to
+be placed in the /etc/facter/facts.d directory for it to run properly. It
+will provide a number of facts about the computer it is running on.
+
+* timemachine_configured
+** 1 if TimeMachine is configured, 0 if not.
+* timemachine_url
+** The backup URL of the first configured TimeMachine volume.
+* timemachine_lastsnapshot
+** The date, as a unix timestamp, of the last successful backup.
+* timemachine_firstsnapshot
+** The date, as a unix timestamp, of the first successful backup.
+* timemachine_snapshotcount
+** The number of backups on the volume.
+
+## Server Plugin
+
+The server plugin provides list information to help you categorize the
+following states of machines:
+
+* TimeMachine Configured
+* TimeMachine not configured
+* Backed up < 7 days ago
+* Backed up between 7 and 28 days ago
+* Backed up > 28 days ago
---
+++ timemachine/client/timemachine_info.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+# Facter script to gather various facts about Time Machine.
+# This script should be placed in the facts.d directory, which on Mac
+# OS X is /etc/facter/facts.d
+#
+
+version=`sw_vers -productVersion | cut -f1-2 -d.`
+
+# Determine if it is configured.
+configured=0
+if [ -e /usr/bin/tmutil ]; then
+  V=`/usr/bin/tmutil destinationinfo | grep "No destinations configured"`
+  if [ -z "$V" ]; then
+    configured=1
+  fi
+fi
+
+# Get the URL used for backups.
+url=""
+if [ $configured -eq 1 ]; then
+  url=`tmutil destinationinfo | grep "^URL" | cut -c17- | head -n1`
+fi
+
+# Get the most recent snapshot date.
+lastsnapshot=""
+if [ $configured -eq 1 ]; then
+  if [ "$version" = "10.6" ]; then
+    lastsnapshot=0
+  elif [ "$version" = "10.7" -o "$version" = "10.8" ]; then
+    lastsnapshot=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:BACKUP_COMPLETED_DATE" /Library/Preferences/com.apple.TimeMachine.plist`
+    lastsnapshot=`date -j -f "%a %b %d %T %Z %Y" "$lastsnapshot" +%s 2>/dev/null`
+  else
+    lastsnapshot=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:SnapshotDates" /Library/Preferences/com.apple.TimeMachine.plist | grep ":" | tail -n1 | sed 's/^ *//g'`
+    lastsnapshot=`date -j -f "%a %b %d %T %Z %Y" "$lastsnapshot" +%s 2>/dev/null`
+  fi
+fi
+
+# Get the oldest snapshot date.
+firstsnapshot=""
+if [ $configured -eq 1 ]; then
+  if [ "$version" = "10.6" ]; then
+    firstsnapshot=0
+  elif [ "$version" = "10.7" -o "$version" = "10.8" ]; then
+    firstsnapshot=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:kCSBackupdOldestCompleteSnapshotDate" /Library/Preferences/com.apple.TimeMachine.plist`
+    firstsnapshot=`date -j -f "%a %b %d %T %Z %Y" "$firstsnapshot" +%s 2>/dev/null`
+  else
+    firstsnapshot=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:SnapshotDates" /Library/Preferences/com.apple.TimeMachine.plist | grep ":" | head -n1 | sed 's/^ *//g'`
+    firstsnapshot=`date -j -f "%a %b %d %T %Z %Y" "$firstsnapshot" +%s 2>/dev/null`
+  fi
+fi
+
+# Get the number of snapshots
+snapshotcount="0"
+if [ $configured -eq 1 ]; then
+  if [ "$version" = "10.6" ]; then
+    snapshotcount=0
+  elif [ "$version" = "10.7" -o "$version" = "10.8" ]; then
+    snapshotcount=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:SnapshotCount" /Library/Preferences/com.apple.TimeMachine.plist`
+  else
+    snapshotcount=`/usr/libexec/PlistBuddy -c "Print :Destinations:0:SnapshotDates" /Library/Preferences/com.apple.TimeMachine.plist | grep -c ":"`
+  fi
+fi
+
+echo "timemachine_configured=$configured"
+echo "timemachine_url=$url"
+echo "timemachine_lastsnapshot=$lastsnapshot"
+echo "timemachine_firstsnapshot=$firstsnapshot"
+echo "timemachine_snapshotcount=$snapshotcount"
