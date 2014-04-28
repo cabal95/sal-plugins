c40244b95ad92b90de364ff78b97a390d83debe7
Daniel Hazelbaker <daniel@blueboxmoon.com>
2014-04-28 13:47:00 -0700

    Update diskerror.rb to account for older systems that store errors in /var/log/kernel.log

--- diskerror/client/diskerror.rb
+++ diskerror/client/diskerror.rb
@@ -2,13 +2,26 @@ require 'facter'
  
 begin
   errors = 0
-  File.open("/var/log/system.log").each_line do |line|
-    begin
-      if ( line.chomp =~ /disk.*: I\/O error/ )
-        errors = 1
-        break
+  if ( File.file?("/var/log/kernel.log") )
+    File.open("/var/log/kernel.log").each_line do |line|
+      begin
+        if ( line.chomp =~ /disk.*: I\/O error/ )
+          errors = 1
+          break
+        end
+      rescue
       end
-    rescue
+    end
+  end
+  if ( File.file?("/var/log/system.log") )
+    File.open("/var/log/system.log").each_line do |line|
+      begin
+        if ( line.chomp =~ /disk.*: I\/O error/ )
+          errors = 1
+          break
+        end
+      rescue
+      end
     end
   end
 end
